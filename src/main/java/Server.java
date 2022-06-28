41a59e79295be0fc1636931f0ae1bd4a29e6d1e1
Kirill Zubkov <kirill131313@gmail.com>
2022-06-28 16:00:32 +0300

    Task2

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,42 +1,102 @@
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.*;
 
-public class Server  {
+public class Server {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final List<String> allowedMethods = List.of(GET, POST);
+    Request request = new Request();
 
-    ExecutorService executorService = Executors.newFixedThreadPool(64);
 
-    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-
-    public void requestClassic(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
-        final var template = Files.readString(filePath);
-        final var content = template.replace(
-                "{time}",
-                LocalDateTime.now().toString()
-        ).getBytes();
-        out.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + content.length + "\r\n" +
-                        "Connection: close\r\n" +
-                        "\r\n"
-        ).getBytes());
-        out.write(content);
-        out.flush();
+    public void serverStart() {
+        try (final var serverSocket = new ServerSocket(9999)) {
+            while (true) {
+                try (
+                        final var socket = serverSocket.accept();
+                        final var in = new BufferedInputStream(socket.getInputStream());
+                        final var out = new BufferedOutputStream(socket.getOutputStream());
+                ) {
+                    //request.getQueryParams(in, out);
+                    request.getQueryParam(in,out, "title");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void badRequest(BufferedOutputStream out) throws IOException {
+
+    public void parse(BufferedInputStream in, BufferedOutputStream out) throws IOException {
+        // лимит на request line + заголовки
+        final var limit = 4096;
+
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+        }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+        }
+
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            badRequest(out);
+        }
+        System.out.println(method);
+
+        final var path = requestLine[1];
+        if (!path.startsWith("/")) {
+            badRequest(out);
+        }
+        System.out.println(path);
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+        System.out.println(headers);
+
+        // для GET тела нет
+        if (!method.equals(GET)) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+
+                final var body = new String(bodyBytes);
+                System.out.println(body);
+            }
+        }
+
         out.write((
-                "HTTP/1.1 404 Not Found\r\n" +
+                "HTTP/1.1 200 OK\r\n" +
                         "Content-Length: 0\r\n" +
                         "Connection: close\r\n" +
                         "\r\n"
@@ -44,71 +104,35 @@ public class Server  {
         out.flush();
     }
 
-    public void requestFile(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
-        final var length = Files.size(filePath);
+    public static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    public static void badRequest(BufferedOutputStream out) throws IOException {
         out.write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: " + mimeType + "\r\n" +
-                        "Content-Length: " + length + "\r\n" +
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
                         "Connection: close\r\n" +
                         "\r\n"
         ).getBytes());
-        Files.copy(filePath, out);
         out.flush();
     }
 
-    public void serverStart(int port) {
-
-        System.out.println("Server started");
-
-        try (var serverSocket = new ServerSocket(port)) {
-            while (true) {
-
-                final var socket = serverSocket.accept();
-                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final var out = new BufferedOutputStream(socket.getOutputStream());
-
-                executorService.execute(() -> {
-                    try {
-                        listen(socket, in, out);
-                        if(socket.isClosed()) {
-                            executorService.shutdown();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                System.out.println("New request completed");
+    // from google guava with modifications
+    static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            return i;
         }
-    }
-
-    public void listen(Socket socket, BufferedReader in, BufferedOutputStream out) throws IOException {
-        // read only request line for simplicity
-        // must be in form GET /path HTTP/1.1
-        final var requestLine = in.readLine();
-        System.out.println("Результат работы потока " + Thread.currentThread().getName() + " : " + requestLine);
-        final var parts = requestLine.split(" ");
-
-        if (parts.length != 3) {
-            socket.close();
-            // just close socket
-        }
-
-        final var path = parts[1];
-        if (!validPaths.contains(path)) {
-            badRequest(out);
-        }
-
-        final var filePath = Path.of(".", "public", path);
-        final var mimeType = Files.probeContentType(filePath);
-
-        // special case for classic
-        if (path.equals("/classic.html")) {
-            requestClassic(out, filePath, mimeType);
-        }
-        requestFile(out, filePath, mimeType);
+        return -1;
     }
 }
